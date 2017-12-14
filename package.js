f18cfc9b288a66ec8a5dd758aaffe9ce211dc19b
Fred Horch <fred@sparkae.com>
2017-12-14 00:22:08 -0500

    Fix github URL
    Now points to meteor mogul.

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Simple binding to Vue.js for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/lwakefield/vue-for-meteor',
+  git: 'https://github.com/meteor-mogul/mogul-vue-dist.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
