00d31e27837bf0b34be45005fae6140f5fa36b2f
Fred Horch <fred@sparkae.com>
2017-12-14 00:19:03 -0500

    Use vue 2.5.10
    Package vue 2.5.10 in meteor.

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'meteormogul:vue-dist',
+  version: '2.5.10',
+  // Brief, one-line summary of the package.
+  summary: 'Simple binding to Vue.js for Meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/lwakefield/vue-for-meteor',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.addFiles('vue.js');
+});
+
+Package.onTest(function(api) {
+});
